cde198a443111d9e0e77a6f7b5c123af05e8fc92
rawipas2 <54341677+rawipas2@users.noreply.github.com>
2020-05-18 11:51:05 +0700

    add

--- src/dog/dogController.ts
+++ src/dog/dogController.ts
@@ -4,6 +4,7 @@ import { Request, Response } from 'express'
 export const getAllDog = async (req: Request, res: Response) => {
 	try {
 		const dogs = await fetchAllDog()
+
 		return res.json(dogs)
 	} catch (error) {
 		return res.status(500).json({})
